31dab63d670c33d03d60052cf5f42b8f32d41a83
root <root@debian.server.home>
2020-06-23 19:29:19 +0100

    Footer fixes

--- footer.php
+++ footer.php
@@ -2,31 +2,31 @@
 
 	<div class="footer-buttons">
 
-		<p>
-			<?php if (get_privacy_policy_url()) :?>
-			<a href="<?=get_privacy_policy_url()?>" class="tooltip" data-tooltip-text="Política de privacidad">
-				<i class="fa fa-user-secret"></i>
-			</a>
-			<?php endif; ?>
+		<?php if (get_privacy_policy_url()) :?>
+		<a href="<?=get_privacy_policy_url()?>" class="tooltip" data-tooltip-text="Política de privacidad">
+			<i class="fa fa-user-secret"></i>
+		</a>
+		<?php endif; ?>
 
-			<?php if (get_page_by_path("/contact")) :?>
-			<a href="/contact" class="tooltip" data-tooltip-text="Contacto">
-				<i class="fa fa-envelope"></i>
-			</a>
-			<?php endif; ?>
+		<?php if (get_page_by_path("/contact")) :?>
+		<a href="/contact" class="tooltip" data-tooltip-text="Contacto">
+			<i class="fa fa-envelope"></i>
+		</a>
+		<?php endif; ?>
 
-			<a href="#site-top" class="tooltip" data-tooltip-text="Volver arriba" style="margin-left: 30px;"> 
-				<i class="fa fa-arrow-up"></i>
-			</a>
-		</p>
+		<a href="#site-top" class="tooltip" data-tooltip-text="Volver arriba" style="margin-left: 30px;"> 
+			<i class="fa fa-arrow-up"></i>
+		</a>
 
 	</div>
 
-	<p style="text-align: left;">
-		<i class="fa fa-copyright"></i> 
-		<?=date('Y')?>
-		Hecho con <a href="https://wordpress.org" target="_blank"><i class="fab fa-wordpress-simple"></i></a> y <a href="https://github.com/SantiMora/myWpLightTheme" target="_blank">mi tema</a>
-	</p>
+	<i class="fa fa-copyright"></i> 
+
+	<?=date('Y')?>
+
+	Hecho con <a href="https://wordpress.org" target="_blank"><i class="fab fa-wordpress-simple"></i></a> y 
+	<a href="https://github.com/SantiMora/myWpLightTheme" target="_blank">mi tema</a>
+
 </footer>
 <?php wp_footer();?>
 </body>
